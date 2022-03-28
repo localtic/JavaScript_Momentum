cd5a77658ba755d9d432ea9d9cb910906e52a141
localtic <gussm150@gmail.com>
2022-03-28 18:04:21 +0900

    Events

--- login/app.js
+++ login/app.js
@@ -1,16 +1,19 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("#login-form input");
+
+const link = document.querySelector("a");
 
 // const loginInput = document.getElementById("#login-form input");
 
-function onLoginBtnClick(){
-    const username = loginInput.value;
-    // if (username === ""){
-    //     alert("Please write your name");
-    // } else if(username.length > 15){
-    //     alert("your name is too long")
-    // }
+function onLoginSubmit(info){
+    info.preventDefault(); // 브라우저의 기본 동작(새로고침)을 막음
+    console.log(info); // event에 대한 정보
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function handleLinkClick(event){
+    console.log(event);
+    alert("clicked"); // alert는 모든 동작을 멈춤
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
--- login/memo.md
+++ login/memo.md
@@ -8,3 +8,6 @@ null, 글자수 제한 등 브라우저에서 구현 가능
 
 form 안의 버튼을 누르면 submit 된다 -> 클릭 안 만들어줘도 됨
 form이 submit될 때마다 새로고침됨
+
+submit할 때, function이 방금 발생한 event에 대한 정보를 argument로 받는다
+MouseEvent(PointerEvent) 유저가 클릭한 위치를 x,y 좌표로 받는다
