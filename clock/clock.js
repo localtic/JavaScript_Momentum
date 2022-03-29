bb1bc21cc35497bc4deb04063b9c8b9c850fd1f2
localtic <gussm150@gmail.com>
2022-03-29 17:30:21 +0900

    padStart, padEnd

--- clock/clock.js
+++ clock/clock.js
@@ -9,7 +9,10 @@ function sayHello() {
 
 function getClock(){
     const date = new Date();
-    clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText =`${hours}:${minutes}:${seconds}`;
 };
 
 getClock() // 화면 로딩되자마자 시간이 뜨게 하려고 호출
--- clock/memo.md
+++ clock/memo.md
@@ -1,6 +1,10 @@
 divide and conquer
 
-interval 매번 일어나야 하는 간격
+interval 반복되는 간격
 setInterval(실행하려는 함수, ms 단위로 간격 설정)
+timeout 이 시간 이후 실행
+setTimeout(실행하려는 함수, ms 단위로 기다리는 시간)
 
-timeout(실행하려는 함수, ms 단위로 기다리는 시간)
+TIP) 개발자들이 흔히 겪는 문제는 이미 JavaScript에 내장돼 있을 것
+"1".padStart(2, "0") //01
+"1".padEnd(2, "0") //10
